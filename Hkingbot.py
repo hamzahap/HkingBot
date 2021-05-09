ada5db16f56a3c1ebef26a98db3911af9d77bbd6
Hamzah Punjabi <hamzahqatar123@gmail.com>
2021-05-09 14:02:03 +0300

    should workkkk

--- Hkingbot.py
+++ Hkingbot.py
@@ -4,10 +4,6 @@ import random
 import discord
 from dotenv import load_dotenv
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
-
 client = discord.Client()
 
 @client.event
