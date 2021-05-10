bb940f874611ffb49ea98f54b9a172b318f1182b
Hamzah Punjabi <hamzahqatar123@gmail.com>
2021-05-10 18:47:56 +0300

    Ettui
    RIP

--- Hkingbot.py
+++ Hkingbot.py
@@ -10,33 +10,11 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    #Mudkiproasts = [
-     #   'Mudkip is literally the worst Pokemon',
-      #  'Mudkip needs to have an opinion on everything.' ,
-       # (
-       #     'Mudkip should stop boasting about his GPA'
-       #     '\nHe really needs help'
-       # ),
-    #]
-    #Zeshaanroasts = [
-    #    'Zeshaan is still shifting lol',
-    #    'What about Zeshaan? that man can\'t leave Mesaid. It\'s been like 3 months now lol'
-    #]
-    #Melvicroasts = [
-    #    'Ay esports trash, go play valorant. Diehard Riot fan'
-    #]
-    #if 'melvic' in message.content.lower():
-    #    response = random.choice(Melvicroasts)
-     #   await message.channel.send(response)
-
-    #if 'mudkip' in message.content.lower():
-     #   response = random.choice(Mudkiproasts)
-    #    await message.channel.send(response)
-   # if 'zeshaan' in message.content.lower() or 'biryani' in message.content.lower():
-       # response = random.choice(Zeshaanroasts)
-      #  await message.channel.send(response)
-
     if 'vibecat' in message.content.lower() or 'catvibe' in message.content.lower() or 'vibe' in message.content.lower():
         await message.channel.send(file=discord.File('catvibe.gif'))
+    if 'kermit' in message.content.lower() or 'kys' in message.content.lower():
+        await message.channel.send(file=discord.File('kermit.gif'))
+    if 'ettu' in message.content.lower():
+        await message.channel.send(file=discord.File('ettu.gif'))
 
 client.run(os.environ['DISCORD_TOKEN'])
