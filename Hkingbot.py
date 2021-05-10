f245a7983235669423e6c48dafb2f11fb9080412
Hamzah Punjabi <hamzahqatar123@gmail.com>
2021-05-10 19:31:22 +0300

    Should search gif now
    quallec gif

--- Hkingbot.py
+++ Hkingbot.py
@@ -1,8 +1,11 @@
 import os
 import random
-
+import giphy_client
 import discord
+from giphy_client.rest import ApiException
+from discord.ext import commands
 
+client = commands.Bot(command_prefix="$")
 client = discord.Client()
 
 @client.event
@@ -16,5 +19,22 @@ async def on_message(message):
         await message.channel.send(file=discord.File('kermit.gif'))
     if 'ettu' in message.content.lower():
         await message.channel.send(file=discord.File('ettu.gif'))
+
+@client.command()
+async def gif(ctx,*,q):
+    giphykey = os.environ['GIPHY_KEY']
+    api_instance = giphy_client.DefaultApi()
+
+    try:
+        api_response = api_instance.gifs_search_get(giphykey, q, limit, rating='r')
+        lst = list(api_response.data)
+        giff = random.choice(lst)
+        emb = discord.Embed(title=q)
+        emb.set_image(url=f'https://media.giphy.com/media/{giff.id}/giphy.gif')
+
+        await ctx.channel.send(embed=emb)
+
+    except ApiException as e:
+        print("Error calling API")
 
 client.run(os.environ['DISCORD_TOKEN'])
