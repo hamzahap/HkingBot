1883a2e203904ff4269c7e1849371147911f6765
Hamzah Punjabi <hamzahqatar123@gmail.com>
2021-05-10 21:37:09 +0300

    ahh

--- Hkingbot.py
+++ Hkingbot.py
@@ -7,17 +7,17 @@ from discord.ext import commands
 
 client = commands.Bot(command_prefix="$")
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if 'vibecat' in message.content.lower() or 'catvibe' in message.content.lower() or 'vibe' in message.content.lower():
-        await message.channel.send(file=discord.File('catvibe.gif'))
-    if 'kermit' in message.content.lower() or 'kys' in message.content.lower():
-        await message.channel.send(file=discord.File('kermit.gif'))
-    if 'ettu' in message.content.lower():
-        await message.channel.send(file=discord.File('ettu.gif'))
+#@client.event
+#async def on_message(message):
+#    if message.author == client.user:
+#        return
+#
+#   if 'vibecat' in message.content.lower() or 'catvibe' in message.content.lower() or 'vibe' in message.content.lower():
+#        await message.channel.send(file=discord.File('catvibe.gif'))
+#    if 'kermit' in message.content.lower() or 'kys' in message.content.lower():
+#        await message.channel.send(file=discord.File('kermit.gif'))
+#    if 'ettu' in message.content.lower():
+#        await message.channel.send(file=discord.File('ettu.gif'))
 
 @client.command()
 async def gif(ctx,*,q):
