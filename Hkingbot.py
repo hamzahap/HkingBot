795c5ff594be8f78f9e229c3b4d017b5d7a19757
Hamzah Punjabi <hamzahqatar123@gmail.com>
2021-05-16 12:27:05 +0300

    Updates

--- Hkingbot.py
+++ Hkingbot.py
@@ -2,38 +2,47 @@ import os
 import random
 import giphy_client
 import discord
-from giphy_client.rest import ApiException
 from discord.ext import commands
+import urllib
+import json
 
-client = commands.Bot(command_prefix="$")
+VIBE_IMAGE = "https://s3.gifyu.com/images/catvibe.gif"
+VIBIER_IMAGE = "https://s3.gifyu.com/images/ezgif.com-gif-maker-174e18faa852a3028.gif"
+VIBIEST_IMAGE = "https://s3.gifyu.com/images/ezgif.com-gif-maker-2664260aedaea9638.gif"
 
-#@client.event
-#async def on_message(message):
-#    if message.author == client.user:
-#        return
-#
-#   if 'vibecat' in message.content.lower() or 'catvibe' in message.content.lower() or 'vibe' in message.content.lower():
-#        await message.channel.send(file=discord.File('catvibe.gif'))
-#    if 'kermit' in message.content.lower() or 'kys' in message.content.lower():
-#        await message.channel.send(file=discord.File('kermit.gif'))
-#    if 'ettu' in message.content.lower():
-#        await message.channel.send(file=discord.File('ettu.gif'))
+client = commands.Bot(command_prefix="!")
 
 @client.command()
-async def gif(ctx,*,q):
+async def vibe(ctx, vibelevel):
+    if vibelevel == 1:
+        await ctx.send(VIBE_IMAGE)
+    elif vibelevel == 2:
+        await ctx.send(VIBIER_IMAGE)
+    else:
+        await ctx.send(VIBIEST_IMAGE)
+
+@client.command()
+async def kys(ctx):
+    await ctx.send(file=discord.File('kermit.gif'))
+
+@client.command()
+async def ettu(ctx):
+    await ctx.send(file=discord.File('ettu.gif'))
+
+@client.command()
+async def gif(ctx, *, q):
     giphykey = os.getenv("GIPHY_KEY")
-    api_instance = giphy_client.DefaultApi()
+    url = "http://api.giphy.com/v1/gifs/search"
+    params = urllib.parse.urlencode({
+    "q": q,
+    "api_key": giphykey,
+    "limit": "25"
+    })
+    with urllib.request.urlopen("".join(url, params)) as response:
+        data = json.loads(response.read())
+    list1 = list(data)
+    giff = random.choice(list1)
 
-    try:
-        api_response = api_instance.gifs_search_get(giphykey, q, limit=25, rating='r')
-        lst = list(api_response.data)
-        giff = random.choice(lst)
-        emb = discord.Embed(title=q)
-        emb.set_image(url=f'https://media.giphy.com/media/{giff.id}/giphy.gif')
-
-        await ctx.channel.send(embed=emb)
-
-    except ApiException as e:
-        print("Error calling API")
+    await ctx.channel.send(giff)
 
 client.run(os.environ['DISCORD_TOKEN'])
