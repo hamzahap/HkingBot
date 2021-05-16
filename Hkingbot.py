929624fe175877746b96cc49a07427e2903bfe33
Hamzah Punjabi <hamzahqatar123@gmail.com>
2021-05-16 13:00:38 +0300

    eh

--- Hkingbot.py
+++ Hkingbot.py
@@ -3,6 +3,7 @@ import random
 import giphy_client
 import discord
 from discord.ext import commands
+from giphy_client.rest import ApiException
 
 VIBE_IMAGE = "https://s3.gifyu.com/images/catvibe.gif"
 VIBIER_IMAGE = "https://s3.gifyu.com/images/ezgif.com-gif-maker-174e18faa852a3028.gif"
@@ -41,7 +42,13 @@ async def gif(ctx, *, q):
     #    data = json.loads(response.read())
     #list1 = list(data)
     #giff = random.choice(list1)
-
-    await ctx.channel.send(random.choice("http://api.giphy.com/v1/gifs/search?q={query}&api_key={giphykey}&limit=25"))
+    #url = "http://api.giphy.com/v1/gifs/search?q={query}&api_key={giphykey}&limit=25"
+    
+    api_instance = giphy_client.DefaultApi()
+    api_response = api_instance.gifs_search_get(giphykey, q, limit=25, rating='r')
+    list1 = list(api_response.data)
+    giff = random.choice(list1)
+    
+    await ctx.channel.send(giff)
 
 client.run(os.environ['DISCORD_TOKEN'])
